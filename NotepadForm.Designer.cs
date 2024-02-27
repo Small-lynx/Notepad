d14bf310bf8a990a4c3ce2f225af1f5f53f64732
Small-lynx <lynxrufus2k4@gmail.com>
2024-02-27 20:58:42 +0300

    Добавьте файлы проекта.

---
+++ NotepadForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Notepad
+{
+    partial class NotepadForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStripNotepad = new System.Windows.Forms.MenuStrip();
+            this.statusStripNotepad = new System.Windows.Forms.StatusStrip();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.timerSpeed = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // menuStripNotepad
+            // 
+            this.menuStripNotepad.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStripNotepad.Location = new System.Drawing.Point(0, 0);
+            this.menuStripNotepad.Name = "menuStripNotepad";
+            this.menuStripNotepad.Size = new System.Drawing.Size(800, 24);
+            this.menuStripNotepad.TabIndex = 0;
+            // 
+            // statusStripNotepad
+            // 
+            this.statusStripNotepad.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStripNotepad.Location = new System.Drawing.Point(0, 428);
+            this.statusStripNotepad.Name = "statusStripNotepad";
+            this.statusStripNotepad.Size = new System.Drawing.Size(800, 22);
+            this.statusStripNotepad.TabIndex = 1;
+            this.statusStripNotepad.Text = "statusStrip1";
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.Location = new System.Drawing.Point(0, 24);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(800, 404);
+            this.textBox.TabIndex = 2;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            this.textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // timerSpeed
+            // 
+            this.timerSpeed.Enabled = true;
+            this.timerSpeed.Interval = 3000;
+            this.timerSpeed.Tick += new System.EventHandler(this.timerSpeed_Tick);
+            // 
+            // NotepadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.statusStripNotepad);
+            this.Controls.Add(this.menuStripNotepad);
+            this.MainMenuStrip = this.menuStripNotepad;
+            this.Name = "NotepadForm";
+            this.Text = "Блокнот";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripNotepad;
+        private System.Windows.Forms.StatusStrip statusStripNotepad;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Timer timerSpeed;
+    }
+}
+
---
+++ NotepadForm.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class NotepadForm : Form
+    {
+        private string fileName;
+        private int countCharacter;
+        public NotepadForm()
+        {
+            InitializeComponent();
+            var menuItem = new ToolStripMenuItem[3]
+            {
+                new ToolStripMenuItem("Файл"),
+                new ToolStripMenuItem("Сохранить"),
+                new ToolStripMenuItem("Вид")
+            };
+            menuItem[0].DropDownItems.Add("Создать", null, newFile_Clik);
+            menuItem[0].DropDownItems.Add("Открыть", null, openFile_Clik);
+            menuItem[0].DropDownItems.Add("Закрыть", null, closeFile_Clik);
+            menuItem[1].DropDownItems.Add("Сохранить", null, saveFile_Clik);
+            menuItem[1].DropDownItems.Add("Сохранить как ...", null, saveAsFile_Clik);
+            menuItem[2].DropDownItems.Add("Шрифт", null, fontFile_Clik);
+            menuItem[2].DropDownItems.Add("Фон", null, backgraundFile_Clik);
+            menuStripNotepad.Items.AddRange(menuItem);
+
+            var statusMenuItem = new ToolStripMenuItem[3]
+            {
+                new ToolStripMenuItem("Строка: Символ: "),
+                new ToolStripMenuItem("Количество символов"),
+                new ToolStripMenuItem("Скорость печати")
+            };
+            statusStripNotepad.Items.AddRange(statusMenuItem);
+            fileName = "";
+            countCharacter = 0;
+        }
+
+        /// <summary>
+        /// Создание файла
+        /// </summary>
+        private void newFile_Clik(object sender, EventArgs e)
+        {
+            saveFile_Clik(sender, e);
+            textBox.Text = "";
+        }
+
+        /// <summary>
+        /// Открытие файла
+        /// </summary>
+        private void openFile_Clik(object sender, EventArgs e)
+        {
+            var openFile = new OpenFileDialog()
+            {
+                Filter = "Текстовые файлы(*.txt)|*.txt"
+            };
+            if (openFile.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            saveFile_Clik(sender, e);
+            fileName = openFile.FileName;
+            Text = $"Блокнот - {fileName}";
+            textBox.Text = System.IO.File.ReadAllText(openFile.FileName);
+        }
+
+        /// <summary>
+        /// Закрытие файла
+        /// </summary>
+        private void closeFile_Clik(object sender, EventArgs e)
+        {
+            var closeResult = MessageBox.Show("Хотители вы сохранить изменения в файле?", "Блокнот",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Exclamation,
+                MessageBoxDefaultButton.Button1);
+            switch (closeResult)
+            {
+                case DialogResult.Cancel:
+                    return;
+                case DialogResult.Yes:
+                    saveFile_Clik(sender, e);
+                    Close();
+                    break;
+                case DialogResult.No:
+                    Close();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Сохранение файла
+        /// </summary>
+        private void saveFile_Clik(object sender, EventArgs e)
+        {
+            if (fileName == "")
+            {
+                saveAsFile_Clik(sender, e);
+            }
+            else
+            {
+                System.IO.File.WriteAllText(fileName, textBox.Text);
+            }
+        }
+
+        /// <summary>
+        /// Сохранение файла в другом месте
+        /// </summary>
+        private void saveAsFile_Clik(object sender, EventArgs e)
+        {
+            var saveFile = new SaveFileDialog()
+            {
+                DefaultExt = "Текстовые файлы(*.txt)|*.txt",
+                Filter = "Текстовые файлы(*.txt)|*.txt|Все файлы(*.*)|*.*",
+                AddExtension = true,
+                OverwritePrompt = true
+            };
+            if (saveFile.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            System.IO.File.WriteAllText(saveFile.FileName, textBox.Text);
+        }
+
+        /// <summary>
+        /// Шрифт файла
+        /// </summary>
+        private void fontFile_Clik(object sender, EventArgs e)
+        {
+            var fontFile = new FontDialog();
+            if (fontFile.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            textBox.Font = fontFile.Font;
+        }
+
+        /// <summary>
+        /// Фон файла
+        /// </summary>
+        private void backgraundFile_Clik(object sender, EventArgs e)
+        {
+            var colorFile = new ColorDialog();
+            if (colorFile.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            textBox.BackColor = colorFile.Color;
+        }
+
+        /// <summary>
+        /// Подсчет символов
+        /// </summary>
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            statusStripNotepad.Items[1].Text = textBox.Text.Length.ToString() + " символов";
+            countCharacter += 1;
+        }
+
+        /// <summary>
+        /// Расчет скорости печати
+        /// </summary>
+        private void timerSpeed_Tick(object sender, EventArgs e)
+        {
+            countCharacter *= 20;
+            statusStripNotepad.Items[2].Text = countCharacter.ToString() + " зн./мин";
+            countCharacter = 0;
+        }
+
+        /// <summary>
+        /// Расчет позиции курсора
+        /// </summary>
+        private void cursorPosition()
+        {
+            var lineIndex = textBox.GetLineFromCharIndex(textBox.SelectionStart);
+            var colIndex = textBox.SelectionStart - textBox.GetFirstCharIndexFromLine(lineIndex);
+            statusStripNotepad.Items[0].Text = $"Столбец: {lineIndex + 1} Стока: {colIndex}";
+        }
+
+        /// <summary>
+        /// Вывод позиции курсора
+        /// </summary>
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            cursorPosition();
+        }
+    }
+}
